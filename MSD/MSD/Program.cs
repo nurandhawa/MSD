390a7b5e153c21c790e1c0143dbccd7c9a6ed76e
Faldoncow <cowFaldon@gmail.com>
2013-10-07 20:45:42 -0700

    Initial commit

---
+++ MSD/MSD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace MSD
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
